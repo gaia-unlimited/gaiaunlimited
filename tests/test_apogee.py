7b469813af959fbc43852685efd1eddb237f1da5
Tristan <tristancantat@gmail.com>
2023-02-27 12:58:28 +0100

    add file to setup.cfg

--- tests/test_apogee.py
+++ tests/test_apogee.py
@@ -2,16 +2,10 @@ from gaiaunlimited.selectionfunctions.apogee import apogee_sf
 from astropy.coordinates import SkyCoord
 
 def test_apogee():
-	'''
-	Due to GithUb actions error this is temporarily replaced with a dummy test.
-	'''
-	if False:
-		c1 = SkyCoord(226,60,frame='galactic',unit='deg')
-		c2 = SkyCoord(228,60,frame='galactic',unit='deg')
-		c3 = SkyCoord(224,60,frame='galactic',unit='deg')
-		assert apogee_sf(11,1,c3) == 0
-		assert apogee_sf(11,1,c1) > 0
-		assert apogee_sf(11,1,c2) > 0
-		assert apogee_sf(11,1,c2) > apogee_sf(11,1,c1)
-	else:
-		assert True
+    c1 = SkyCoord(226,60,frame='galactic',unit='deg')
+    c2 = SkyCoord(228,60,frame='galactic',unit='deg')
+    c3 = SkyCoord(224,60,frame='galactic',unit='deg')
+    assert apogee_sf(11,1,c3) == 0
+    assert apogee_sf(11,1,c1) > 0
+    assert apogee_sf(11,1,c2) > 0
+    assert apogee_sf(11,1,c2) > apogee_sf(11,1,c1)
