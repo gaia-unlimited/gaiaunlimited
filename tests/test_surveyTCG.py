864e8c3ed1651dd309e561bb611a8cd04499ce95
TristanCantatGaudin <tristancantat@gmail.com>
2023-03-01 10:47:29 +0100

    Merge three map modes into one class (#26)
    * merge hpx7/multi/patch
    
    * test all three modes
    
    * avoid name overlap (class named DR3SelectionFunction clashed with class from dr3sf, renamed it to DR3SelectionFunctionTCG)

--- tests/test_surveyTCG.py
+++ tests/test_surveyTCG.py
@@ -1,9 +1,42 @@
 from gaiaunlimited.selectionfunctions.surveyTCG import DR3SelectionFunctionTCG_hpx7
+from astropy.coordinates import SkyCoord
+from gaiaunlimited.selectionfunctions.surveyTCG import DR3SelectionFunctionTCG
+import numpy as np
 
 
 def test_tcg():
-
     try:
         DR3SelectionFunctionTCG_hpx7()
     except:
         assert False, "Failed"
+
+
+def test_multi():
+    mapMulti = DR3SelectionFunctionTCG("multi")
+    c_0_0 = mapMulti.query(
+        SkyCoord(ra=[0], dec=[0], unit="deg", frame="icrs"), np.array([21])
+    )
+    c_ngc5139 = mapMulti.query(
+        SkyCoord(ra=[201.8], dec=[-47.5], unit="deg", frame="icrs"), np.array([21])
+    )
+    assert c_0_0 > c_ngc5139
+
+
+def test_hpx7():
+    mapHpx7 = DR3SelectionFunctionTCG("hpx7")
+    c_0_0 = mapHpx7.query(
+        SkyCoord(ra=[0], dec=[0], unit="deg", frame="icrs"), np.array([21])
+    )
+    c_ngc5139 = mapHpx7.query(
+        SkyCoord(ra=[201.8], dec=[-47.5], unit="deg", frame="icrs"), np.array([21])
+    )
+    assert c_0_0 > c_ngc5139
+
+
+def test_patch():
+    coord_patch = SkyCoord(ra=200, dec=-47, unit="deg", frame="icrs")
+    mapPatch = DR3SelectionFunctionTCG("patch", coord_patch, radius=0.3, min_points=20)
+    c_200_m47 = mapPatch.query(
+        SkyCoord(ra=[200], dec=[-47], unit="deg", frame="icrs"), np.array([21])
+    )
+    assert c_200_m47 > 0
