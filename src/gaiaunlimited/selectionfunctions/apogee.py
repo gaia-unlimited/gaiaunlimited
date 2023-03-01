cfc51a36c311e72365a5836d7744d47095bc1ce9
TristanCantatGaudin <tristancantat@gmail.com>
2023-03-01 10:47:29 +0100

    Merge three map modes into one class (#26)
    * merge hpx7/multi/patch
    
    * test all three modes
    
    * avoid name overlap (class named DR3SelectionFunction clashed with class from dr3sf, renamed it to DR3SelectionFunctionTCG)

--- src/gaiaunlimited/selectionfunctions/apogee.py
+++ src/gaiaunlimited/selectionfunctions/apogee.py
@@ -6,17 +6,18 @@ import numpy as np
 from pathlib import Path
 import pkg_resources
 
-def apogee_sf(apparentH: float,unreddenedJK: float, position: SkyCoord):
+
+def apogee_sf(apparentH: float, unreddenedJK: float, position: SkyCoord):
     """
     Returns the probability that a star of given intrinsic colour (J-K)_0
     and apparent magnitude H and given position on the sky was targeted by
     APOGEE or APOGEE-2.
-    
+
     Args:
         apparentH (:float): the apparent magnitude H of the source.
-        
+
         unreddenedJK (:float): the intrinsic (J-K) colour of the source.
-    
+
         position (:obj:`astropy.coordinates.SkyCoord`): Sky coordinates at which
             to compute the selection function.
 
@@ -24,104 +25,118 @@ def apogee_sf(apparentH: float,unreddenedJK: float, position: SkyCoord):
         The selection function as either a float (between 0 and 1) or a numpy array
         of floats, with the same length as the input.
     """
-    #Read the precomputed table with the details for each field:
-    apogee_frac_path = pkg_resources.resource_filename('gaiaunlimited', 'data/apogee_sampling_fractions.csv')
+    # Read the precomputed table with the details for each field:
+    apogee_frac_path = pkg_resources.resource_filename(
+        "gaiaunlimited", "data/apogee_sampling_fractions.csv"
+    )
     if not Path(apogee_frac_path).resolve().is_file():
-        datadir_path = pkg_resources.resource_filename('gaiaunlimited', 'data/')
+        datadir_path = pkg_resources.resource_filename("gaiaunlimited", "data/")
         import os
-        print( datadir_path )
-        print( os.listdir(datadir_path) )
-        print( os.listdir(datadir_path+"/..") )
-        print( Path(apogee_frac_path).resolve() )
+
+        print(datadir_path)
+        print(os.listdir(datadir_path))
+        print(os.listdir(datadir_path + "/.."))
+        print(Path(apogee_frac_path).resolve())
         raise ValueError("Precomputed APOGEE selection fraction file not found.")
-    tApogeeSF = Table.read( apogee_frac_path )
-    allFieldNames = sorted(set(tApogeeSF['FIELD']))
-    allFieldCenterL = [ tApogeeSF['GLON'][ tApogeeSF['FIELD']==f ][0] for f in allFieldNames]
-    allFieldCenterB = [ tApogeeSF['GLAT'][ tApogeeSF['FIELD']==f ][0] for f in allFieldNames]
-    allFieldNames = np.array([ tApogeeSF['FIELD'][ tApogeeSF['FIELD']==f ][0] for f in allFieldNames])
-    allFieldRadii = np.array([ tApogeeSF['RADIUS'][ tApogeeSF['FIELD']==f ][0] for f in allFieldNames])
-    coord_centres = SkyCoord(l=allFieldCenterL*units.deg,b=allFieldCenterB*units.deg, frame='galactic')
-    
+    tApogeeSF = Table.read(apogee_frac_path)
+    allFieldNames = sorted(set(tApogeeSF["FIELD"]))
+    allFieldCenterL = [
+        tApogeeSF["GLON"][tApogeeSF["FIELD"] == f][0] for f in allFieldNames
+    ]
+    allFieldCenterB = [
+        tApogeeSF["GLAT"][tApogeeSF["FIELD"] == f][0] for f in allFieldNames
+    ]
+    allFieldNames = np.array(
+        [tApogeeSF["FIELD"][tApogeeSF["FIELD"] == f][0] for f in allFieldNames]
+    )
+    allFieldRadii = np.array(
+        [tApogeeSF["RADIUS"][tApogeeSF["FIELD"] == f][0] for f in allFieldNames]
+    )
+    coord_centres = SkyCoord(
+        l=allFieldCenterL * units.deg, b=allFieldCenterB * units.deg, frame="galactic"
+    )
+
     try:
         nbStars = len(apparentH)
         inputIsList = True
     except:
         nbStars = 1
-        #the input is a single star
+        # the input is a single star
         apparentH = [apparentH]
         unreddenedJK = [unreddenedJK]
         position = [position]
         inputIsList = False
-        
+
     to_return = []
-    
+
     for i in range(len(apparentH)):
-        
-        #For each star we store the selection fraction and the A_K fraction
+        # For each star we store the selection fraction and the A_K fraction
         # of each field it belongs to.
         selectionFraction = []
         akFraction = []
-        
-        if apparentH[i]<5 or apparentH[i]>13.8:
-            #we know these stars are never picked for targeting
-            to_return.append( 0 )
-            continue
 
+        if apparentH[i] < 5 or apparentH[i] > 13.8:
+            # we know these stars are never picked for targeting
+            to_return.append(0)
+            continue
 
         sep = position[i].separation(coord_centres)
-        fieldOfSource = allFieldNames[ sep.deg < allFieldRadii ]
+        fieldOfSource = allFieldNames[sep.deg < allFieldRadii]
 
         if len(fieldOfSource) == 0:
-            #this location is not covered by any APOGEE(-2) grid pointing
-            to_return.append( 0 )
+            # this location is not covered by any APOGEE(-2) grid pointing
+            to_return.append(0)
             continue
-            
+
         # Now we know the source is inside at least one APOGEE(-2) pointing
         # The vast majority of the sky is only covered by one pointing,
         # but we still need to handle the rare cases, so we iterate over the
         # pointings the source is a part of:
         for f in fieldOfSource:
-            tF = tApogeeSF[ tApogeeSF['FIELD']==f ]
+            tF = tApogeeSF[tApogeeSF["FIELD"] == f]
 
-            if tF['cohort'][0]=='short_blue':
-                #this is an APOGEE-2 field with colour binning
-                #we need to find out if our source is considered blue or red
-                JKlimit = tF['JK0max'][0]
+            if tF["cohort"][0] == "short_blue":
+                # this is an APOGEE-2 field with colour binning
+                # we need to find out if our source is considered blue or red
+                JKlimit = tF["JK0max"][0]
                 if unreddenedJK[i] < JKlimit:
-                    tF = tF[[0,2,4]]
+                    tF = tF[[0, 2, 4]]
                 else:
-                    tF = tF[[1,3,5]]
+                    tF = tF[[1, 3, 5]]
 
-            if apparentH[i] < tF['Hmin'][0] or apparentH[i] > tF['Hmax'][2]:
-                #the star is too bright or too faint
-                to_return[-1].append( 0 )
+            if apparentH[i] < tF["Hmin"][0] or apparentH[i] > tF["Hmax"][2]:
+                # the star is too bright or too faint
+                to_return[-1].append(0)
 
-            #bright enough to be short cohort:
-            elif apparentH[i] < tF['Hmax'][0]:
-                #short cohort
-                selectionFraction.append( tF['fracSampling'][0] )
-                akFraction.append( tF['fracAK'][0] )
+            # bright enough to be short cohort:
+            elif apparentH[i] < tF["Hmax"][0]:
+                # short cohort
+                selectionFraction.append(tF["fracSampling"][0])
+                akFraction.append(tF["fracAK"][0])
 
-            #medium cohort
-            elif apparentH[i] < tF['Hmax'][1]   and    apparentH[i] > tF['Hmin'][1]:
-                #medium cohort
-                selectionFraction.append( tF['fracSampling'][1] )
-                akFraction.append( tF['fracAK'][1] )
+            # medium cohort
+            elif apparentH[i] < tF["Hmax"][1] and apparentH[i] > tF["Hmin"][1]:
+                # medium cohort
+                selectionFraction.append(tF["fracSampling"][1])
+                akFraction.append(tF["fracAK"][1])
 
-            #long cohort
-            elif apparentH[i] < tF['Hmax'][2]   and  apparentH[i] < tF['Hmin'][2]:
-                #long cohort
-                selectionFraction.append( tF['fracSampling'][2] )
-                akFraction.append( tF['fracAK'][2] )
+            # long cohort
+            elif apparentH[i] < tF["Hmax"][2] and apparentH[i] < tF["Hmin"][2]:
+                # long cohort
+                selectionFraction.append(tF["fracSampling"][2])
+                akFraction.append(tF["fracAK"][2])
 
-        if len(selectionFraction)==1:
-            to_return.append( selectionFraction[0]*akFraction[0] )            
+        if len(selectionFraction) == 1:
+            to_return.append(selectionFraction[0] * akFraction[0])
         else:
-            to_return.append( (sum(selectionFraction) - np.prod(selectionFraction)*0.5*sum(akFraction)) )
-    
+            to_return.append(
+                (
+                    sum(selectionFraction)
+                    - np.prod(selectionFraction) * 0.5 * sum(akFraction)
+                )
+            )
+
     if inputIsList:
         return to_return
     else:
         return to_return[0]
-    
-    
